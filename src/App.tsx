b6f6f0d4f9c53f3b27261592c494b94565684e8a
H4ck3rxPK <cisco2023123@gmail.com>
2025-07-07 08:33:26 +0000

    Initial commit: OSINT Corporate Recon Platform

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import OSINTDashboard from './components/OSINTDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <OSINTDashboard />
+    </div>
+  );
+}
+
+export default App;
